af0ac7af001c28924c8cacfe97ce8da6120cade5
marlesz <leszczuk.marta@gmail.com>
2015-11-25 16:36:09 +0100

    Wykonano lab5

--- bayes.py
+++ bayes.py
@@ -1,14 +1,23 @@
+# -*- coding: utf-8 -*-
+
 import csv
 import random
 import math
- 
+import numpy
+
 def loadCsv(filename):
     """zaimportuj plik .csv jako listę list liczb typu float"""
-    dataset = None
-    # TODO: uzupełnij
-	return dataset
- 
- def testLoadCsv():
+    f = open(filename)
+    lines = f.readlines()
+    f.close()
+    dataset = [[] for line in lines]
+    for nr, line in enumerate(lines):
+        fields = line.strip().split(',')
+        for i in fields:
+            dataset[nr].append(float(i))
+    return dataset
+
+def testLoadCsv():
     filename = 'pima-indians-diabetes.data.csv'
     dataset = loadCsv(filename)
     assert(len(dataset))
@@ -17,9 +26,12 @@ def loadCsv(filename):
 def splitDataset(dataset, splitRatio):
     """podziel zbiór danych na zbiory: uczący i testowy"""
     trainSet = []
-    testSet = []
-    # TODO: uzupełnij
-	return [trainSet, testSet]
+    testSet = list(dataset)
+    while len(trainSet) <  int(len(dataset) * splitRatio):
+	    index = random.randrange(len(testSet))
+	    trainSet.append(testSet.pop(index))
+    return [trainSet, testSet]
+
     
 def testSplitDataset():
     dataset = [[1], [2], [3], [4], [5]]
@@ -28,12 +40,17 @@ def testSplitDataset():
     assert(train)
     assert(test)
     print('Split {0} rows into train with {1} and test with {2}').format(len(dataset), train, test)
-    
+
+
 def separateByClass(dataset):
     """Rozdziel zbiór uczący według klasy (v[-1]) przypisanej wektorowi cech v"""
-	separated = {}
-    # TODO: uzupełnij
-	return separated
+    separated = {}
+    for i in range(len(dataset)):
+        v = dataset[i]
+        if (v[-1] not in separated):
+            separated[v[-1]] = []
+        separated[v[-1]].append(v)
+    return separated
 
 def testSeparateByClass(): 
     dataset = [[1,20,1], [2,21,0], [3,22,1]]
@@ -43,27 +60,28 @@ def testSeparateByClass():
 
 def mean(numbers):
     """Oblicz średnią arytmetyczną z listy danych"""
-	mean = 0
-    # TODO: uzupełnij
+    mean = 0
+    mean = numpy.mean(numbers)
     return mean
-    
+
 def stdev(numbers):
     """Oblicz odchylenie standardowe z listy danych"""
-	avg = mean(numbers)
-	variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
-	return math.sqrt(variance)
-    
-def testMeanAndStdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+    return math.sqrt(variance)
+
+def testMeanAndStdev():
     numbers = [1,2,3,4,5]
     assert(mean)
     assert(stdev)
     print('Summary of {0}: mean={1}, stdev={2}').format(numbers, mean(numbers), stdev(numbers))
- 
+
 def summarize(dataset):
-	summaries = [""" TODO: uzupełnij""" for attribute in zip(*dataset)]
-	return summaries
-    
-def testSummarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    del summaries[-1] #ostatnia kolumna to numer klasy
+    return summaries
+
+def testSummarize():
     dataset = [[1,20,0], [2,21,1], [3,22,0]]
     summary = summarize(dataset)
     assert(summary)
@@ -78,8 +96,8 @@ def summarizeByClass(dataset):
  
 def calculateProbability(x, mean, stdev):
     """Oblicz prawdopodobieństwo"""
-	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
 
 def testCalculateProbability():
     x = 71.5
@@ -90,10 +108,16 @@ def testCalculateProbability():
     print('Probability of belonging to this class: {0}').format(probability)
 
 def calculateClassProbabilities(summaries, inputVector):
-"""Oblicz prawdopodobieństwo występowania klas"""
-	probabilities = {}
-# TODO: uzupełnij
+    """Oblicz prawdopodobieństwo występowania klas"""
+    probabilities = {}
+    for classValue, classSummaries in summaries.iteritems():
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            iv = inputVector[i]
+            probabilities[classValue] = calculateProbability(iv, mean, stdev)
 	return probabilities
+
+
 
 def testCalculateClassProbabilities():    
     summaries = {0:[(1, 0.5)], 1:[(20, 5.0)]}
@@ -101,26 +125,33 @@ def testCalculateClassProbabilities():
     probabilities = calculateClassProbabilities(summaries, inputVector)
     assert(probabilities)
     print('Probabilities for each class: {0}').format(probabilities)
-			
+
+
 def predict(summaries, inputVector):
-"""Dokonaj predykcji jednego elementu zbioru danych wg danych prawdopodobieństw"""
-	probabilities = calculateClassProbabilities(summaries, inputVector)
-	bestLabel, bestProb = None, -1
-    # TODO: uzupełnij
+    """Dokonaj predykcji jednego elementu zbioru danych wg danych prawdopodobieństw"""
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.iteritems():
+        if bestLabel is None or probability > bestProb:
+            bestLabel = classValue
+            bestProb = probability
 	return bestLabel
-    
+
+
 def testPredict():
-    summaries = {'A':[(1, 0.5)], 'B':[(20, 5.0)]}
+    summaries = {'A': [(1, 0.5)], 'B': [(20, 5.0)]}
     inputVector = [1.1, '?']
     result = predict(summaries, inputVector)
-    assert(result == 'A')
+    assert (result == 'A')
     print('Prediction: {0}').format(result)
- 
+
+
 def getPredictions(summaries, testSet):
-"""Dokonaj predykcji dla wszystkich elementów w zbiorze danych"""
-	predictions = []
-# TODO: uzupełnij
-	return predictions
+    """Dokonaj predykcji dla wszystkich elementów w zbiorze danych"""
+    predictions = []
+    for i in range(len(testSet)):
+        predictions.append(predict(summaries, testSet[i]))
+    return predictions
 
 def testGetPredictions():
     summaries = {'A':[(1, 0.5)], 'B':[(20, 5.0)]}
@@ -129,10 +160,13 @@ def testGetPredictions():
     print('Predictions: {0}').format(predictions)
     
 def getAccuracy(testSet, predictions):
-"""Oblicz dokładność przewidywań"""
-	correct = 0
-	# TODO: uzupełnij
-	return (correct/float(len(testSet))) * 100.0
+    """Oblicz dokładność przewidywań"""
+    correct = 0
+    for i in range(len(testSet)):
+        if testSet[i][-1] == predictions[i]:
+            correct +=1
+    return (correct/float(len(testSet))) * 100.0
+
 
 def testGetAccuracy():
     testSet = [[1,1,1,'a'], [2,2,2,'a'], [3,3,3,'b']]
@@ -141,15 +175,26 @@ def testGetAccuracy():
     print('Accuracy: {0}').format(accuracy)
     
 def main():
-	filename = 'pima-indians-diabetes.data.csv'
-	splitRatio = 0.67
-	dataset = loadCsv(filename)
-	trainingSet, testSet = splitDataset(dataset, splitRatio)
-	print('Split {0} rows into train={1} and test={2} rows').format(len(dataset), len(trainingSet), len(testSet))
-	# prepare model
-	summaries = summarizeByClass(trainingSet)
-	# test model
-	predictions = getPredictions(summaries, testSet)
-	accuracy = getAccuracy(testSet, predictions)
-	print('Accuracy: {0}%').format(accuracy)
+    filename = 'pima-indians-diabetes.data.csv'
+    splitRatio = 0.67
+    dataset = loadCsv(filename)
+    trainingSet, testSet = splitDataset(dataset, splitRatio)
+    print('Split {0} rows into train={1} and test={2} rows').format(len(dataset), len(trainingSet), len(testSet))
+    # prepare model
+    summaries = summarizeByClass(trainingSet)
+    # test model
+    predictions = getPredictions(summaries, testSet)
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: {0}%').format(accuracy)
  
+
+#testLoadCsv()
+#testSplitDataset()
+#testSeparateByClass()
+#testMeanAndStdev()
+#testSummarize()
+#testCalculateClassProbabilities()
+#testPredict()
+#testGetPredictions()
+#testGetAccuracy()
+main()
